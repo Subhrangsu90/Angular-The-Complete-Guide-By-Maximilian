8ed0977f5e704e92828f5580dee3611b619cff10
Subhrangsu90 <subhrangsubera@outlook.com>
2024-05-26 01:23:25 +0530

    till lec 28 done

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
